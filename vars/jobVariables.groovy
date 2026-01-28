b1c0ea627b178764bec12822103aa5635f0973c8
altyapistaj <altyapi_staj@proton.me>
2026-01-28 13:36:37 +0300

    test commit 2

--- vars/jobVariables.groovy
+++ vars/jobVariables.groovy
@@ -1,12 +1,13 @@
 def call(String JOB_NAME){
+
     return [
             projectName : 'demo-library-test',
             jobName : JOB_NAME.tokenize('/')[1],
             branch : "main",
             pom : 'pom.xml',
 
-
             organizationName : "altyapistaj",
+
             gitAdressAndName : JOB_NAME.tokenize('/')[1]
     ]
 
